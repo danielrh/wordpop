2eb4cf0ba18a546de24aab9f67f8a4cee8de53d5
Daniel Reiter Horn <danielrh@dropbox.com>
2017-07-27 10:50:11 -0700

    depend on a real random number generator and eliminate duplicates before merging in the last gen lru with the cur gen lru

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -1,3 +1,5 @@
+extern crate rand;
+use rand::{Rng, SeedableRng};
 use std::fs::File;
 use std::io::{Read,ErrorKind,Result, Write, BufWriter};
 use std::io;
@@ -105,11 +107,38 @@ SlicableTrait!(Word24,24);
 
 #[derive(Copy,Clone,PartialEq,Eq)]
 struct Entry<Word:Sliceable+Ord+Clone+Copy> {
-    count: i32,
+    internal_count: i32,
     word: Word,
     block_id:u8
 }
-
+impl<Word:Sliceable+Ord+Clone+Copy> Entry<Word> {
+    fn new(word:&Word,block_id:u8, is_random_insertion:bool) ->Self {
+        Entry::<Word> {
+            internal_count: if is_random_insertion {-1} else {1},
+            word:word.clone(),
+            block_id:block_id,
+        }
+    }
+    fn count(&self) -> i32 {
+        self.internal_count.abs()
+    }
+    fn increment(&mut self, delta: i32) {
+        if self.internal_count < 0 {
+            self.internal_count -= delta;
+        } else {
+            self.internal_count += delta;
+        }
+    }
+    fn reset_count_to_one(&mut self) {
+        self.internal_count = 1;
+    }
+    fn set_nonrandom_flag(&mut self) {
+        self.internal_count = self.internal_count.abs();
+    }
+    fn is_random_insert(&self) -> bool {
+        self.internal_count < 0
+    }
+}
 impl<Word:Sliceable+Ord+Copy> PartialOrd for Entry<Word> {
     fn partial_cmp(&self, other:&Self) -> Option<cmp::Ordering> {
         self.word.partial_cmp(&other.word)
@@ -121,21 +150,21 @@ impl<Word:Sliceable+Ord+Copy> Ord for Entry<Word> {
     }
 }
 
-fn init_silly_rand() -> u64{
-    18446744073709551557u64
+fn init_silly_rand() -> rand::ChaChaRng {
+    rand::ChaChaRng::from_seed(&[((18446744073709551557u64 >> 16) >> 16) as u32, (18446744073709551557u64 & 0xffffffff) as u32][..])
 }
-fn silly_rand(state:&mut u64) -> u64{
-    *state = state.wrapping_mul(6364136223846793005u64);
-    *state += 1442695040888963407u64;
-    return *state;
+fn silly_rand(state:&mut rand::ChaChaRng, low: u64, high: u64) -> u64{
+    return state.gen_range(low, high);
 }
-fn find_candidate<Word:Sliceable+Ord+Default+Copy>(lru:&Vec<Entry<Word>>, rand_state:&mut u64) -> usize {
-    let mut best_index = silly_rand(rand_state) as usize % lru.len();
-    let mut best_count = lru[best_index].count.abs();
-    let num_candidates = 4;
+
+fn find_candidate<Word:Sliceable+Ord+Default+Copy>(lru:&Vec<Entry<Word>>,
+                                                   rand_state:&mut rand::ChaChaRng,
+                                                   num_candidates:usize) -> usize {
+    let mut best_index = silly_rand(rand_state, 0, lru.len() as u64) as usize;
+    let mut best_count = lru[best_index].count();
     for _ in 0..num_candidates {
-        let index = silly_rand(rand_state) as usize % lru.len();
-        let count = lru[best_index].count.abs();
+        let index = silly_rand(rand_state, 0, lru.len() as u64) as usize;
+        let count = lru[best_index].count();
         if count < best_count {
             best_count = count;
             best_index = index;
@@ -148,7 +177,7 @@ struct LRU<Word:Sliceable+Ord+Copy> {
     lru:Vec<Entry<Word>>,
     cur:Vec<Entry<Word>>,
     cur_index: usize,
-    rand_state: u64,
+    rand_state: rand::ChaChaRng,
 }
 
 #[cfg(not(feature="ascii"))]
@@ -190,23 +219,23 @@ fn starts_with_constant<Word:Sliceable>(w:&Word) -> bool {
 impl<Word:Sliceable+Ord+Default+Copy> LRU<Word> {
     fn new(num_entries: usize) -> Self {
         LRU::<Word>{
-            lru:vec![Entry::<Word>{count:0,word:Word::default(),block_id:255};num_entries],
-            cur:vec![Entry::<Word>{count:0, word:Word::default(), block_id:255};num_entries],
+            lru:vec![Entry::<Word>{internal_count:0,word:Word::default(),block_id:255};num_entries],
+            cur:vec![Entry::<Word>{internal_count:0, word:Word::default(), block_id:255};num_entries],
             cur_index:0,
             rand_state:init_silly_rand(),
         }
     }
     fn print<W:Write>(&mut self, w:&mut W) -> io::Result<()>{
-        self.evict(true);
+        self.evict(true, 0);
         let mut buf = [0u8;512];
         for entry in self.lru.iter() {
-            if entry.count == 0 {
+            let count = entry.count();
+            if count <= 1 {
                 continue;
             }
             let mut cur_index = print_encode(&mut buf, &entry.word);
             buf[cur_index] = ' ' as u8;
             cur_index += 1;
-            let count = entry.count;
             let mut digit = 1000000000;
             while digit >= 10 {
                 if count >= digit {
@@ -223,21 +252,40 @@ impl<Word:Sliceable+Ord+Default+Copy> LRU<Word> {
         }
         Ok(())
     }
-    fn evict(&mut self, fin:bool) {
+    fn eliminate_duplicates(&mut self) {
+        let mut validated_index = 0usize;
+        assert!(self.cur_index <= self.cur.len());
+        for index in 1..cmp::min(self.cur_index, self.cur.len()) {
+            if self.cur[validated_index].word == self.cur[index].word {
+                if self.cur[validated_index].block_id != self.cur[index].block_id {
+                    let del = self.cur[index].count();
+                    self.cur[validated_index].increment(del);
+                }
+            } else {
+                validated_index += 1;
+                let entry = self.cur[index].clone();
+                self.cur[validated_index] = entry;
+            }
+        }
+        self.cur_index = validated_index + 1;
+    }
+    fn evict(&mut self, fin:bool, num_candidates: usize) {
         self.cur[..self.cur_index].sort();
+        self.eliminate_duplicates();
         let mut index = 0;
         for entry in self.cur[..self.cur_index].iter() {
             let mut found = false;
             while index < self.lru.len() {
-                if self.lru[index].count != -1 && // just forcibly inserted
-                    (self.lru[index].word >= entry.word || self.lru[index].count == 0) {
+                if self.lru[index].is_random_insert() == false && // just forcibly inserted
+                    (self.lru[index].word >= entry.word || self.lru[index].count() == 0) {
                         if self.lru[index].word == entry.word {
                             if self.lru[index].block_id != entry.block_id {
-                                self.lru[index].count += 1;
+                                self.lru[index].set_nonrandom_flag();
+                                self.lru[index].increment(entry.count());
                                 self.lru[index].block_id = entry.block_id;
                             }
                             found = true;
-                        } else if self.lru[index].count == 0{
+                        } else if self.lru[index].count() == 0 {
                             self.lru[index] = entry.clone();
                             found = true;
                         } else {
@@ -249,25 +297,19 @@ impl<Word:Sliceable+Ord+Default+Copy> LRU<Word> {
             }
             if fin == false && !found {
                 // randomly insert
-                let index = find_candidate(&self.lru, &mut self.rand_state);
-                self.lru[index] = Entry::<Word>{
-                    count:-1,
-                    word:entry.word,
-                    block_id:entry.block_id,
-                }
+                let index = find_candidate(&self.lru, &mut self.rand_state, num_candidates);
+                self.lru[index] = Entry::<Word>::new(&entry.word, entry.block_id, true);
             }
         }
         if !fin {
             for entry in self.lru.iter_mut() {
-                if entry.count == -1 {
-                    entry.count = 1;
-                }
+                entry.set_nonrandom_flag();
             }
         }
         self.lru.sort();
         self.cur_index = 0;
     }
-    fn add(&mut self, block_id:u8, block:&[u8;4096* 1024], index: usize) {
+    fn add(&mut self, block_id:u8, block:&[u8;4096* 1024], index: usize, num_candidates: usize) {
         {
             let mut dst = &mut self.cur[self.cur_index];
             let len = dst.word.slice().len();
@@ -275,12 +317,12 @@ impl<Word:Sliceable+Ord+Default+Copy> LRU<Word> {
             if ends_with_constant(&dst.word) ||starts_with_constant(&dst.word) {
                 return; // lets not bother using this
             }
-            dst.count = 1;
+            dst.reset_count_to_one();
             dst.block_id = block_id;
             self.cur_index += 1;
         }
         if self.cur_index == self.cur.len() {
-            self.evict(false);
+            self.evict(false, num_candidates);
             assert_eq!(self.cur_index, 0);
         }
     }
@@ -346,7 +388,7 @@ fn evaluate_biggest<Word:Sliceable+Default+Copy+Ord>(_:&() ,_lru: &LRU<Word>, _:
     Word::default().slice().len()
 }
 fn eof(_:&()){}
-fn add_block(block_id: u8, block:&[u8;4096 * 1024], size:usize, lru:&mut FullLRU, rle_max: usize) {
+fn add_block(block_id: u8, block:&[u8;4096 * 1024], size:usize, lru:&mut FullLRU, rle_max: usize, num_candidates: usize) {
     assert!(size <= 4096 * 1024);
     let max_len = evaluate_biggest(&lru.3, &lru.24, &lru.25);
     assert_eq!(max_len, 24);//known a priori...but the above checks it since EOF comes after lru.24
@@ -367,27 +409,27 @@ fn add_block(block_id: u8, block:&[u8;4096 * 1024], size:usize, lru:&mut FullLRU
         }
         rle_char = val;
 
-        lru.4.add(block_id, &block, index);
-        lru.5.add(block_id, &block, index);
-        lru.6.add(block_id, &block, index);
-        lru.7.add(block_id, &block, index);
-        lru.8.add(block_id, &block, index);
-        lru.9.add(block_id, &block, index);
-        lru.10.add(block_id, &block, index);
-        lru.11.add(block_id, &block, index);
-        lru.12.add(block_id, &block, index);
-        lru.13.add(block_id, &block, index);
-        lru.14.add(block_id, &block, index);
-        lru.15.add(block_id, &block, index);
-        lru.16.add(block_id, &block, index);
-        lru.17.add(block_id, &block, index);
-        lru.18.add(block_id, &block, index);
-        lru.19.add(block_id, &block, index);
-        lru.20.add(block_id, &block, index);
-        lru.21.add(block_id, &block, index);
-        lru.22.add(block_id, &block, index);
-        lru.23.add(block_id, &block, index);
-        lru.24.add(block_id, &block, index);
+        lru.4.add(block_id, &block, index, num_candidates);
+        lru.5.add(block_id, &block, index, num_candidates);
+        lru.6.add(block_id, &block, index, num_candidates);
+        lru.7.add(block_id, &block, index, num_candidates);
+        lru.8.add(block_id, &block, index, num_candidates);
+        lru.9.add(block_id, &block, index, num_candidates);
+        lru.10.add(block_id, &block, index, num_candidates);
+        lru.11.add(block_id, &block, index, num_candidates);
+        lru.12.add(block_id, &block, index, num_candidates);
+        lru.13.add(block_id, &block, index, num_candidates);
+        lru.14.add(block_id, &block, index, num_candidates);
+        lru.15.add(block_id, &block, index, num_candidates);
+        lru.16.add(block_id, &block, index, num_candidates);
+        lru.17.add(block_id, &block, index, num_candidates);
+        lru.18.add(block_id, &block, index, num_candidates);
+        lru.19.add(block_id, &block, index, num_candidates);
+        lru.20.add(block_id, &block, index, num_candidates);
+        lru.21.add(block_id, &block, index, num_candidates);
+        lru.22.add(block_id, &block, index, num_candidates);
+        lru.23.add(block_id, &block, index, num_candidates);
+        lru.24.add(block_id, &block, index, num_candidates);
         eof(&lru.25);
     }
 }
@@ -421,12 +463,28 @@ fn main() {
     let mut block = [0u8;4096 * 1024];
     let mut lru = FullLRU::default();
     let mut block_id = 0;
+    let mut rle_max = 4;
+    let mut num_candidates = 16;
     for argument in env::args().skip(1) {
+        if argument.starts_with("-c") {
+            num_candidates = argument.trim_matches('-').trim_matches('c').parse::<i64>().unwrap() as usize;
+        }
+        if argument.starts_with("-r") {
+            rle_max = argument.trim_matches('-').trim_matches('r').parse::<i64>().unwrap() as usize;
+        }
+    }
+    for argument in env::args().skip(1) {
+        if argument.starts_with("-c") {
+            continue;
+        }
+        if argument.starts_with("-r") {
+            continue;
+        }
         let _ = writeln!(std::io::stderr(), "Processing {}", argument);
         let file = File::open(argument).unwrap();
         let _ = std::io::stderr().flush();
         match read_all(&file, &mut block[..]) {
-            Ok(size) => add_block(block_id, &block, size, &mut lru, 4),
+            Ok(size) => add_block(block_id, &block, size, &mut lru, rle_max, num_candidates),
             Err(err) => panic!(err),
         }
         block_id += 1;
